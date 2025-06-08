7eb34ac25b1ead691e01f05a069e9ef03fa45783
Ziad Ghoraba <Eng.ZiadGhoraba@gmail.com>
2025-06-09 00:05:58 +0300

    Initial Commit

---
+++ Original/Original/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Original.Models;
+
+namespace Original.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpPut]
+        public IActionResult Index()
+        {
+            return Content("Hello from HomeController Index!");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Original/Original/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Original
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy.WithOrigins("http://127.0.0.1:5500/Frontend.html")   
+                          .WithHeaders("Content-Type", "Authorization", "X-Requested-With")  
+                          .WithMethods("GET", "POST", "PUT");  
+                });
+            });
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.UseCors("AllowFrontend");
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+
+        }
+    }
+}
